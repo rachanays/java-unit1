6f636fe6d824e9e8cabfba365805502ecae11c01
rachanays <rachanasrinivas956@gmail.com>
2025-11-27 11:31:01 +0530

    Create ElseIfDemo.java for temperature conditions
    Added a Java program to demonstrate conditional statements based on temperature.

---
+++ files/ElseIfDemo.java
@@ -0,0 +1,15 @@
+class ElseIfDemo {
+public static void main(String[] args) {
+int temperature = 25;
+
+Programs to Demonstrate Conditional Statements in
+Java
+
+if (temperature > 35)
+System.out.println("It's too hot outside.");
+else if (temperature > 20)
+System.out.println("The weather is pleasant.");
+else
+System.out.println("It's quite cold.");
+}
+}
